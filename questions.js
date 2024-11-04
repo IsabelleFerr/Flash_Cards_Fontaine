1abe512d1c1ee089b44dfd78a7824f9ab52fcf7a
IsabelleFerr <168565002+IsabelleFerr@users.noreply.github.com>
2024-11-04 19:49:41 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,6 @@
+const questions = [
+    { question: "Qual a profissão de Neuvillette em Fontaine?", answer: "Juiz" },
+    { question: "O que a Furina mais gosta de comer?", answer: "Doces" },
+    { question: "Quem mais chorou na missão de Fontaine?", answer: "Neuvillette, Navia e Furina" },
+    { question: "Qual o Fatui principal em Fontaine?", answer: "Arlecchino" }
+];
